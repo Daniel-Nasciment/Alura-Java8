d4362b191354eeb7338e4cb9306229e7a20fbbea
Daniel <daniel.fecaf@outlook.com>
2022-01-24 20:16:45 -0300

    Trabalhando com Stream

---
+++ src/TestaCursos.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestaCursos {
+
+	public static void main(String[] args) {
+
+		List<Curso> cursos = new ArrayList<>();
+
+		Curso c1 = new Curso("Java8", 10);
+		Curso c2 = new Curso("HTML", 20);
+		Curso c3 = new Curso("C#", 30);
+		Curso c4 = new Curso("Angular", 50);
+
+		cursos.add(c4);
+		cursos.add(c2);
+		cursos.add(c3);
+		cursos.add(c1);
+
+		cursos.sort(Comparator.comparing(Curso::getAlunos)); // method reference
+
+		cursos.forEach(c -> System.out.println(c)); // lambda expression
+		
+		// STREAM NÃO É UMA COLECTION
+		// FAZER ALTERAÇÕES EM UM STREAM NÃO ALTERA A COLEÇÃO ORIGINAL
+		
+		int sum = cursos.stream()
+		.filter(c -> c.getAlunos() > 20)
+		.mapToInt(Curso::getAlunos)
+		.sum();
+		
+		System.out.println(sum);
+		
+		cursos.forEach(c -> System.out.println(c));
+
+	}
+
+}
